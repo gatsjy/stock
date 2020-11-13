7079759f7182b9a8ac5fbd56fb3b1ac12bf05130
Gatsjy <gkswndks123@naver.com>
2020-11-13 22:43:34 +0900

    2020-11-13, 한주안, 주식 봇 만들기 5

--- src/test/java/hossa/stock/stock/StockApplicationTests.java
+++ src/test/java/hossa/stock/stock/StockApplicationTests.java
@@ -19,10 +19,8 @@ class StockApplicationTests {
 
 	@Test
 	public void sendMsgTest(){
-/*
-		TelegramBot bot = new TelegramBot("");
-
-		SendMessage request = new SendMessage("", "wtf!!! hello bot!!")
+		TelegramBot bot = new TelegramBot("1308465026:AAHOrMFyULrupxEnhkPIsNjGJ0o-4uF0q7U");
+		SendMessage request = new SendMessage("729845849", "wtf!!! hello bot!!")
 				.parseMode(ParseMode.HTML)
 				.disableWebPagePreview(true)
 				.disableNotification(false);
@@ -30,7 +28,6 @@ class StockApplicationTests {
 		SendResponse sendResponse = bot.execute(request);
 		boolean ok = sendResponse.isOk();
 		Message message = sendResponse.message();
-*/
 
 	}
 
